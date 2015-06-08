1f4515ff7950cab382e8b7ce5aa5b9eb2b366a35
Clayton Kinder <ClaytonAlanKinder@gmail.com>
2015-06-08 11:56:23 -0400

    Refactored code into object notation. Committed to master.

--- data.js
+++ data.js
@@ -41,213 +41,194 @@ var albums = [
 ];
 
 ///////////////////////////////////
-// VARIABLES
-///////////////////////////////////
-
-var firstImages = fetchFirstImageOfAlbum();
-var templates = {};
-var $allPages = $('body > div');
-var $albumsPage = $('#albumsPage');
-var $albumPage = $('#albumPage');
-var $photoPage = $('#photoPage');
-var activeAlbum;
-var activePhoto;
-
-///////////////////////////////////
 // FETCHERS
 ///////////////////////////////////
 
-function fetchFirstImageOfAlbum() {
-  var firstImages = [];
+var fetch = {
+  firstImageOfAlbum: function() {
+    var firstImages = [];
 
-  albums.forEach(function(el, idx, list){
-    var image = {};
-    image.title = el.photos[0].title;
-    image.imageTag = el.photos[0].imageTag;
-    firstImages.push(image);
-  });
-  return firstImages;
-}
-
-function fetchActiveAlbum() {
-  var fetchedActiveAlbum;
-  for (var i=0; i<albums.length; i++){
-    if (albums[i].title === activeAlbum) {
-      fetchedActiveAlbum = albums[i];
-    }
-  };
-  return fetchedActiveAlbum;
-}
-
-function fetchActivePhoto() {
-  var fetchedActivePhoto;
-  for (var i=0; i<albums.length; i++){
-    for (var j=0; j<albums[i].photos.length; j++) {
-      if (albums[i].photos[j].title === activePhoto) {
-        fetchedActivePhoto = albums[i].photos[j];
+    albums.forEach(function(el, idx, list){
+      var image = {};
+      image.title = el.photos[0].title;
+      image.imageTag = el.photos[0].imageTag;
+      firstImages.push(image);
+    });
+    return firstImages;
+  },
+  activeAlbum: function() {
+    var fetchedActiveAlbum;
+    for (var i=0; i<albums.length; i++){
+      if (albums[i].title === activeAlbum) {
+        fetchedActiveAlbum = albums[i];
       }
-    }
-  };
-  return fetchedActivePhoto;
+    };
+    return fetchedActiveAlbum;
+  },
+  activePhoto: function() {
+    var fetchedActivePhoto;
+    for (var i=0; i<albums.length; i++){
+      for (var j=0; j<albums[i].photos.length; j++) {
+        if (albums[i].photos[j].title === activePhoto) {
+          fetchedActivePhoto = albums[i].photos[j];
+        }
+      }
+    };
+    return fetchedActivePhoto;
+  }
 }
 
 ///////////////////////////////////
 // CONSTRUCTORS
 ///////////////////////////////////
 
-// Builds the album blocks that display on AlbumsPage
-function albumsPageBlockConstructor() {
-  for (i=0; i<firstImages.length; i++) {
-    var constructStr = "";
-    constructStr += '<div class="albumsPageBlock" rel="'
-    constructStr += albums[i].title;
-    constructStr += '">';
-    constructStr += '<div class="albumsPageBlockImage">';
-    constructStr += firstImages[i].imageTag;
-    constructStr += '</div>';
-    constructStr += '<div class="albumsPageBlockTitle"><h3>';
-    constructStr += albums[i].title;
-    constructStr += '</h3></div></div>';
-    $('#albumsPageWrapper').append(constructStr);
-  }
-}
-
-// Builds the nav links that display on AlbumPage
-function albumPageNavLinkConstructor() {
-  for (i=0; i<albums.length; i++) {
-    var constructStr = "";
-    constructStr += '<a href="#" rel="';
-    constructStr += albums[i].title;
-    constructStr += '">';
-    constructStr += albums[i].title;
-    constructStr += '</a>';
-    $('#albumPageNav nav').append(constructStr);
-  }
-}
-
-// Builds the main content that displays on AlbumPage
-function albumPageMainConstructor() {
-  $('#albumPageMain').html("");
-  var constructStr = "";
-  var fetchedActiveAlbum = fetchActiveAlbum();
-  var blocks = "";
-
-  for (var i=0; i<fetchedActiveAlbum.photos.length; i++) {
-    blocks += '<div class="albumPageGalleryBlock" rel="';
-    blocks += fetchedActiveAlbum.photos[i].title;
-    blocks += '">';
-    blocks += '<div class="albumPageGalleryBlockImage">';
-    blocks += fetchedActiveAlbum.photos[i].imageTag;
-    blocks += '</div>';
-    blocks += '<div class="albumPageGalleryBlockTitle"><h3>';
-    blocks += fetchedActiveAlbum.photos[i].title;
-    blocks += '</h3></div></div>';
-  }
-
-  constructStr += '<div id="albumPageTitle">';
-  constructStr += '<div id="albumPageTitleText"><h1>';
-  constructStr += fetchedActiveAlbum.title;
-  constructStr += '</h1></div></div>';
-  constructStr += '<div id="albumPageGallery">';
-  constructStr += '<div id="albumPageGalleryWrapper">';
-  constructStr += blocks;
-  constructStr += '</div></div>';
-  $('#albumPageMain').append(constructStr);
-}
-
-// Builds the photoPage depending on which photo from albumPage is clicked
-function photoPageConstructor(actPho) {
-  $('#photoPage').html("");
-  var constructStr = "";
-  var fetchedActiveAlbum = fetchActiveAlbum();
-  if (actPho !== undefined) {
-    var fetchedActivePhoto = actPho;
-  } else {
-    var fetchedActivePhoto = fetchActivePhoto();
-  }
-
-
-  constructStr += '<div id="photoPageNavWrapper">';
-  constructStr += '<div id="photoPageBackWrapper">';
-  constructStr += '<a id="photoPageBackBlock" href="#">';
-  constructStr += '<div id="photoPageBackIcon"><i class="fa fa-chevron-left"></i></div>';
-  constructStr += '<div id="photoPageBackText">';
-  constructStr += 'Back to ' + fetchedActiveAlbum.title;
-  constructStr += '</div></a></div>';
-  constructStr += '<div id="photoPageTitleBlock"><h1>';
-  constructStr += photoPageTitleConstructor(fetchedActivePhoto);
-  constructStr += '</h1></div></div>';
-  constructStr += '<div id="photoPagePhotoWrapper">';
-  constructStr += photoPagePhotoBlockConstructor();
-  constructStr += '</div>';
-
-  $('#photoPage').append(constructStr);
-
-  $('#photoPageBackBlock').on('click', function(e){
-    e.preventDefault();
-    albumPageMainConstructor();
-    $allPages.removeClass('active');
-    $albumPage.addClass('active');
-  });
-
-
-}
-
-// Builds the title that displays on a single photo page
-function photoPageTitleConstructor(actTitle) {
-  var constructStr = "";
-  var fetchedActivePhoto = fetchActivePhoto();
-
-  if (actTitle !== undefined) {
-    if (typeof actTitle === 'object') {
-      constructStr += actTitle.title;
-    } else {
-      constructStr += actTitle;
+var constructor = {
+  // Builds the album blocks that display on AlbumsPage
+  albumsPageBlock: function() {
+    for (i=0; i<firstImages.length; i++) {
+      var constructStr = "";
+      constructStr += '<div class="albumsPageBlock" rel="'
+      constructStr += albums[i].title;
+      constructStr += '">';
+      constructStr += '<div class="albumsPageBlockImage">';
+      constructStr += firstImages[i].imageTag;
+      constructStr += '</div>';
+      constructStr += '<div class="albumsPageBlockTitle"><h3>';
+      constructStr += albums[i].title;
+      constructStr += '</h3></div></div>';
+      $('#albumsPageWrapper').append(constructStr);
     }
-  } else {
-    constructStr += fetchedActivePhoto.title;
-  }
-  return constructStr;
-}
-
-//  Builds the block that displays the single photo itself
-function photoPagePhotoBlockConstructor(actPho) {
-  $('#photoPagePhotoWrapper').html("");
-  var constructStr = "";
-  var fetchedActiveAlbum = fetchActiveAlbum();
-
-  if (actPho !== undefined) {
-    var fetchedActivePhoto = actPho;
-
-    if (typeof actPho === 'string') {
-      activePhoto = actPho;
-    } else {
-      activePhoto = actPho.title;
+  },
+  // Builds the nav links that display on AlbumPage
+  albumPageNavLink: function() {
+    for (i=0; i<albums.length; i++) {
+      var constructStr = "";
+      constructStr += '<a href="#" rel="';
+      constructStr += albums[i].title;
+      constructStr += '">';
+      constructStr += albums[i].title;
+      constructStr += '</a>';
+      $('#albumPageNav nav').append(constructStr);
     }
+  },
+  // Builds the main content that displays on AlbumPage
+  albumPageMain: function() {
+    $('#albumPageMain').html("");
+    var constructStr = "";
+    var fetchedActiveAlbum = fetch.activeAlbum();
+    var blocks = "";
 
-    constructStr += '<div id="photoPagePhotoBlock" rel="';
-    constructStr += activePhoto;
-    constructStr += '">';
-    constructStr += '<div id="prevPhoto" class="prevPhoto"><i class="fa fa-chevron-left"></i></div>';
     for (var i=0; i<fetchedActiveAlbum.photos.length; i++) {
-      if (fetchedActiveAlbum.photos[i].title === activePhoto) {
-        constructStr += fetchedActiveAlbum.photos[i].imageTag;
-      }
+      blocks += '<div class="albumPageGalleryBlock" rel="';
+      blocks += fetchedActiveAlbum.photos[i].title;
+      blocks += '">';
+      blocks += '<div class="albumPageGalleryBlockImage">';
+      blocks += fetchedActiveAlbum.photos[i].imageTag;
+      blocks += '</div>';
+      blocks += '<div class="albumPageGalleryBlockTitle"><h3>';
+      blocks += fetchedActiveAlbum.photos[i].title;
+      blocks += '</h3></div></div>';
     }
-    constructStr += '<div id="nextPhoto" class="nextPhoto"><i class="fa fa-chevron-right"></i></div>';
+
+    constructStr += '<div id="albumPageTitle">';
+    constructStr += '<div id="albumPageTitleText"><h1>';
+    constructStr += fetchedActiveAlbum.title;
+    constructStr += '</h1></div></div>';
+    constructStr += '<div id="albumPageGallery">';
+    constructStr += '<div id="albumPageGalleryWrapper">';
+    constructStr += blocks;
+    constructStr += '</div></div>';
+    $('#albumPageMain').append(constructStr);
+  },
+  // Builds the photoPage depending on which photo from albumPage is clicked
+  photoPage: function(actPho) {
+    $('#photoPage').html("");
+    var constructStr = "";
+    var fetchedActiveAlbum = fetch.activeAlbum();
+    if (actPho !== undefined) {
+      var fetchedActivePhoto = actPho;
+    } else {
+      var fetchedActivePhoto = fetch.activePhoto();
+    }
+
+    constructStr += '<div id="photoPageNavWrapper">';
+    constructStr += '<div id="photoPageBackWrapper">';
+    constructStr += '<a id="photoPageBackBlock" href="#">';
+    constructStr += '<div id="photoPageBackIcon"><i class="fa fa-chevron-left"></i></div>';
+    constructStr += '<div id="photoPageBackText">';
+    constructStr += 'Back to ' + fetchedActiveAlbum.title;
+    constructStr += '</div></a></div>';
+    constructStr += '<div id="photoPageTitleBlock"><h1>';
+    constructStr += constructor.photoPageTitle(fetchedActivePhoto);
+    constructStr += '</h1></div></div>';
+    constructStr += '<div id="photoPagePhotoWrapper">';
+    constructStr += constructor.photoPagePhotoBlock();
     constructStr += '</div>';
 
-    $('#photoPagePhotoWrapper').append(constructStr);
-  } else {
-    fetchedActivePhoto = fetchActivePhoto();
-    constructStr += '<div id="photoPagePhotoBlock" rel="';
-    constructStr += fetchedActivePhoto.title;
-    constructStr += '">';
-    constructStr += '<div id="prevPhoto" class="prevPhoto"><i class="fa fa-chevron-left"></i></div>';
-    constructStr += fetchedActivePhoto.imageTag;
-    constructStr += '<div id="nextPhoto" class="nextPhoto"><i class="fa fa-chevron-right"></i></div>';
-    constructStr += '</div>';
+    $('#photoPage').append(constructStr);
 
-     return constructStr;
+    $('#photoPageBackBlock').on('click', function(e){
+      e.preventDefault();
+      constructor.albumPageMain();
+      $allPages.removeClass('active');
+      $albumPage.addClass('active');
+    });
+  },
+  // Builds the title that displays on a single photo page
+  photoPageTitle: function(actTitle) {
+    var constructStr = "";
+    var fetchedActivePhoto = fetch.activePhoto();
+
+    if (actTitle !== undefined) {
+      if (typeof actTitle === 'object') {
+        constructStr += actTitle.title;
+      } else {
+        constructStr += actTitle;
+      }
+    } else {
+      constructStr += fetchedActivePhoto.title;
+    }
+    return constructStr;
+  },
+  //  Builds the block that displays the single photo itself
+  photoPagePhotoBlock: function(actPho) {
+    $('#photoPagePhotoWrapper').html("");
+    var constructStr = "";
+    var fetchedActiveAlbum = fetch.activeAlbum();
+
+    if (actPho !== undefined) {
+      var fetchedActivePhoto = actPho;
+
+      if (typeof actPho === 'string') {
+        activePhoto = actPho;
+      } else {
+        activePhoto = actPho.title;
+      }
+
+      constructStr += '<div id="photoPagePhotoBlock" rel="';
+      constructStr += activePhoto;
+      constructStr += '">';
+      constructStr += '<div id="prevPhoto" class="prevPhoto"><i class="fa fa-chevron-left"></i></div>';
+      for (var i=0; i<fetchedActiveAlbum.photos.length; i++) {
+        if (fetchedActiveAlbum.photos[i].title === activePhoto) {
+          constructStr += fetchedActiveAlbum.photos[i].imageTag;
+        }
+      }
+      constructStr += '<div id="nextPhoto" class="nextPhoto"><i class="fa fa-chevron-right"></i></div>';
+      constructStr += '</div>';
+
+      $('#photoPagePhotoWrapper').append(constructStr);
+    } else {
+      fetchedActivePhoto = fetch.activePhoto();
+      constructStr += '<div id="photoPagePhotoBlock" rel="';
+      constructStr += fetchedActivePhoto.title;
+      constructStr += '">';
+      constructStr += '<div id="prevPhoto" class="prevPhoto"><i class="fa fa-chevron-left"></i></div>';
+      constructStr += fetchedActivePhoto.imageTag;
+      constructStr += '<div id="nextPhoto" class="nextPhoto"><i class="fa fa-chevron-right"></i></div>';
+      constructStr += '</div>';
+
+      return constructStr;
+    }
   }
 }
--- photo-album.js
+++ photo-album.js
@@ -1,13 +1,30 @@
+///////////////////////////////////
+// VARIABLES
+///////////////////////////////////
+
+var firstImages = fetch.firstImageOfAlbum();
+var templates = {};
+var $allPages = $('body > div');
+var $albumsPage = $('#albumsPage');
+var $albumPage = $('#albumPage');
+var $photoPage = $('#photoPage');
+var activeAlbum;
+var activePhoto;
+
+///////////////////////////////////
+// ON READY
+///////////////////////////////////
+
 $(document).ready(function(){
 
-  albumsPageBlockConstructor();
-  albumPageNavLinkConstructor();
+  constructor.albumsPageBlock();
+  constructor.albumPageNavLink();
 
   // Transitions from AlbumsPage to AlbumPage with selected album intact.
   $('#albumsPageWrapper').on('click', '.albumsPageBlock', function(e){
     e.preventDefault();
     activeAlbum = $(this).attr('rel');
-    albumPageMainConstructor();
+    constructor.albumPageMain();
 
     $allPages.removeClass('active');
     $albumPage.addClass('active');
@@ -23,7 +40,7 @@ $(document).ready(function(){
   $('#albumPageNav nav a').on('click', function(e){
     e.preventDefault();
     activeAlbum = $(this).attr('rel');
-    albumPageMainConstructor();
+    constructor.albumPageMain();
     $('#albumPageNav nav a').removeClass('currentAlbum');
     $(this).addClass('currentAlbum');
   });
@@ -32,73 +49,68 @@ $(document).ready(function(){
   $('body').on('click', '.albumPageGalleryBlock', function(e){
     e.preventDefault();
     activePhoto = $(this).attr('rel');
-    photoPageConstructor();
+    constructor.photoPage();
 
     $allPages.removeClass('active');
     $photoPage.addClass('active');
   });
 
-  $('body').on('click', '#photoPagePhotoBlock > div', function(e){
+  // Views the previous photo within the album
+  $('body').on('click', '.prevPhoto', function(e){
     e.preventDefault();
-
-
-    if ($(this).hasClass('prevPhoto')) {
-      // Breaks name of activePhoto into array
-      if (typeof activePhoto === 'string') {
-        var activePhotoArray = activePhoto.split("");
-      } else if (typeof activePhoto === 'object') {
-        var activePhotoArray = activePhoto.title.split("");
-      }
-      // Removes last character from activePhotoArray
-      var popped = activePhotoArray.pop();
-      // Subtracts 1 from the number removed in the last step
-      var poppedMinus = (Number(popped) - 1);
-      // If activePhoto is less than 1, it loops around to the end of the array, in this case it's 6.
-      if (poppedMinus < 1) {
-        for (var i=0; i<albums.length; i++) {
-          if (activeAlbum === albums[i].title) {
-            poppedMinus = albums[i].photos.length;
-          }
-        }
-      }
-
-      // Pushes the newly subtracted number onto the end of the array
-      var newActivePhoto = activePhotoArray.push(poppedMinus);
-      // Joins the array back into a string
-      newActivePhoto = activePhotoArray.join("");
-      activePhoto = newActivePhoto;
-      photoPageConstructor(newActivePhoto);
-
-    } else if ($(this).hasClass('nextPhoto')) {
-      // Breaks name of activePhoto into array
-      if (typeof activePhoto === 'string') {
-        var activePhotoArray = activePhoto.split("");
-      } else if (typeof activePhoto === 'object') {
-        var activePhotoArray = activePhoto.title.split("");
-      }
-      // Removes last character from activePhotoArray
-      var popped = activePhotoArray.pop();
-      // Adds 1 from the number removed in the last step
-      var poppedPlus = (Number(popped) + 1);
-      // If activePhoto is greater than the length of the currentAlbum, it loops around to the beginning of the array.
-      if (poppedPlus > albums.length) {
-        for (var i=0; i<albums.length; i++) {
-          if (activeAlbum === albums[i].title) {
-            poppedPlus = 1;
-          }
-        }
-      }
-
-      // Pushes the newly subtracted number onto the end of the array
-      var newActivePhoto = activePhotoArray.push(poppedPlus);
-      // Joins the array back into a string
-      newActivePhoto = activePhotoArray.join("");
-
-      activePhoto = newActivePhoto;
-      photoPageConstructor(newActivePhoto);
+    // Breaks name of activePhoto into array
+    if (typeof activePhoto === 'string') {
+      var activePhotoArray = activePhoto.split("");
+    } else if (typeof activePhoto === 'object') {
+      var activePhotoArray = activePhoto.title.split("");
     }
+    // Removes last character from activePhotoArray
+    var popped = activePhotoArray.pop();
+    // Subtracts 1 from the number removed in the last step
+    var poppedMinus = (Number(popped) - 1);
+    // If activePhoto is less than 1, it loops around to the end of the array, in this case it's 6.
+    if (poppedMinus < 1) {
+      for (var i=0; i<albums.length; i++) {
+        if (activeAlbum === albums[i].title) {
+          poppedMinus = albums[i].photos.length;
+        }
+      }
+    }
+    // Pushes the newly subtracted number onto the end of the array
+    var newActivePhoto = activePhotoArray.push(poppedMinus);
+    // Joins the array back into a string
+    newActivePhoto = activePhotoArray.join("");
+    activePhoto = newActivePhoto;
+    constructor.photoPage(newActivePhoto);
   });
 
+  // Views the next photo within the album
+  $('body').on('click', '.nextPhoto', function(e){
+    e.preventDefault();
+    // Breaks name of activePhoto into array
+    if (typeof activePhoto === 'string') {
+      var activePhotoArray = activePhoto.split("");
+    } else if (typeof activePhoto === 'object') {
+      var activePhotoArray = activePhoto.title.split("");
+    }
+    // Removes last character from activePhotoArray
+    var popped = activePhotoArray.pop();
+    // Adds 1 from the number removed in the last step
+    var poppedPlus = (Number(popped) + 1);
+    // If activePhoto is greater than the length of the currentAlbum, it loops around to the beginning of the array.
+    if (poppedPlus > albums.length) {
+      for (var i=0; i<albums.length; i++) {
+        if (activeAlbum === albums[i].title) {
+          poppedPlus = 1;
+        }
+      }
+    }
 
-
+    // Pushes the newly subtracted number onto the end of the array
+    var newActivePhoto = activePhotoArray.push(poppedPlus);
+    // Joins the array back into a string
+    newActivePhoto = activePhotoArray.join("");
+    activePhoto = newActivePhoto;
+    constructor.photoPage(newActivePhoto);
+  });
 });
